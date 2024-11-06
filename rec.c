a9b20649e2508da3ad0a27b184d7bf9832fa7108
Heisenberg1209 <babhishek770@gmail.com>
2024-11-06 14:07:53 +0530

    hello world

--- rec.c
+++ rec.c
@@ -1,19 +1,5 @@
 #include<stdio.h>
 int main()
 {
-  int arr[15],i,sum=0,avg;
-  for(i=0;i<=14;i++)
-  {
-    printf("enter the number : \n");
-    scanf("%d",&arr[i]);
-    
-  }
-for(i=0;i<=14;i++)
-{
-    printf("the %d element of the array is %d \n",i,arr[i]);
-     sum=sum+arr[i];
-}
-  printf("the sum of the element in array is : %d\n",sum);
-   avg= sum/15;
-  printf("the average of the element in array is : %d\n",avg);
+    printf("hello world");
 }
